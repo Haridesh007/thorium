eab02bab6d2dd4ca76fa86e63481ad7a4c8f75a1
unknown <harideshkumar111@gmail.com>
2022-02-26 16:00:03 +0530

    querry-paramas

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const router = express.Router();
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+   },
+   {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+   },
+   {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+   },
+   {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+   },
+   {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+   }
+   ]
 
 
 router.get("/persons-query" , function(req, res) {
